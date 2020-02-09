35831a85cc46e5559788a73491ff4b7c2f826954
Lillian Grassin-Drake <lilgrassin@gmail.com>
2020-02-08 21:44:49 -0500

    Initial commit

---
+++ LogMe/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LogMe.Services;
+using LogMe.Views;
+
+namespace LogMe
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
